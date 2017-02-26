c4186017a03b983534a60e41e2959b35533ebca3
eizesazake <eizesazake@yahoo.com>
2017-02-26 17:08:03 -0300

    - implementing bitcoinj

---
+++ src/main/java/apkt/opreturn/OpReturnRunnable.java
@@ -0,0 +1,58 @@
+package apkt.opreturn;
+
+import java.security.AccessControlContext;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class OpReturnRunnable {
+
+    private static CountDownLatch startupLatch = new CountDownLatch(1);
+    private static AtomicInteger pendingRunnables = new AtomicInteger(0);
+    private final static Object runLaterLock = new Object();
+
+    private static void waitForStart() {
+        // If the startup runnable has not yet been called, then wait it.
+        // Note that we check the count before calling await() to avoid
+        // the try/catch which is unnecessary after startup.
+        if (startupLatch.getCount() > 0) {
+            try {
+                startupLatch.await();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public static void runLater(final Runnable r) {
+        runLater(r, false);
+    }
+
+    private static void runLater(final Runnable r, boolean exiting) {
+        pendingRunnables.incrementAndGet();
+        waitForStart();
+
+        synchronized (runLaterLock) {
+            if (!exiting) {
+                // Don't schedule a runnable after we have exited the toolkit
+                pendingRunnables.decrementAndGet();
+                return;
+            }
+
+            final AccessControlContext acc = AccessController.getContext();
+                try {
+                    AccessController.doPrivileged((PrivilegedAction<Void>) () ->{
+                        r.run();
+                        return null;
+                    }, acc);
+                } finally {
+                    pendingRunnables.decrementAndGet();
+                }
+        }
+    }
+
+}
+
+
+
---
+++ src/main/java/apkt/service/OpReturnService.java
@@ -0,0 +1,149 @@
+package apkt.service;
+
+
+import apkt.opreturn.OpReturnRunnable;
+import com.google.common.util.concurrent.Service;
+import org.bitcoinj.core.*;
+import org.bitcoinj.kits.WalletAppKit;
+import org.bitcoinj.params.TestNet3Params;
+import org.bitcoinj.script.ScriptBuilder;
+import org.bitcoinj.utils.BriefLogFormatter;
+import org.bitcoinj.wallet.DeterministicSeed;
+import org.bitcoinj.wallet.SendRequest;
+import org.bitcoinj.wallet.Wallet;
+import org.bitcoinj.wallet.listeners.WalletChangeEventListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class OpReturnService {
+    
+//    public static NetworkParameters params = MainNetParams.get();
+    public static NetworkParameters params = TestNet3Params.get();
+    private static final String TWININGS = "Twinings".replaceAll("[^a-zA-Z0-9.-]", "_") + "-"
+            + params.getPaymentProtocolId();
+
+    public static WalletAppKit bitcoin;
+
+    public static void write(String msg) throws IOException {
+
+        if (!bitcoin.isRunning()){
+            return;
+        }
+        
+        // Make log output concise.
+        BriefLogFormatter.init();
+        // Create the app kit. It won't do any heavyweight initialization until after we start it.
+        setupWalletKit(null);
+
+        if (bitcoin.isChainFileLocked()) {
+            return;
+        }
+        bitcoin.addListener(new Service.Listener() {
+
+            @Override
+            public void running() {
+                super.running();
+                try {
+                    writeTx();
+                } catch (IOException ex) {
+                    Logger.getLogger(OpReturnService.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (InsufficientMoneyException ex) {
+                    Logger.getLogger(OpReturnService.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+        }, OpReturnRunnable::runLater);
+        bitcoin.startAsync();
+    }
+
+    public static void setupWalletKit(DeterministicSeed seed) {
+        // If seed is non-null it means we are restoring from backup.
+        bitcoin = new WalletAppKit(params, new File("."), TWININGS) {
+            @Override
+            protected void onSetupCompleted() {
+                // Don't make the user wait for confirmations for now, as the intention is they're sending it
+                // their own money!
+                bitcoin.wallet().allowSpendingUnconfirmedTransactions();
+
+            }
+        };
+        
+        bitcoin.setBlockingStartup(false);
+        
+        if (seed != null){
+            bitcoin.restoreWalletFromSeed(seed);
+        }
+
+    }
+
+    private static void writeTx() throws IOException, InsufficientMoneyException {
+        
+        Address address = bitcoin.wallet().currentReceiveAddress();
+        byte[] b = "17-01-2017 Mane, dobrodosao! vole te tata i mama. Gospodi pomiluj nas.".getBytes("UTF-8");
+        // Create a tx with an OP_RETURN output
+        Transaction tx = new Transaction(params);
+        tx.addOutput(Coin.ZERO, ScriptBuilder.createOpReturnScript(b));
+
+        System.out.println("before: " + bitcoin.wallet().getBalance().toString());
+
+        // Send it to the Bitcoin network
+        bitcoin.wallet().sendCoins(SendRequest.forTx(tx));
+
+        System.out.println("after: " + bitcoin.wallet().getBalance().toString());
+
+        bitcoin.wallet().addChangeEventListener(OpReturnRunnable::runLater, new WalletChangeEventListener() {
+            @Override
+            public void onWalletChanged(Wallet wallet) {
+                update(wallet);
+            }
+        });
+
+    }
+
+    public static void update(Wallet wallet) {
+        Coin coin = wallet.getBalance();
+        System.out.println("balance: " + wallet.getBalance().toString());
+        Address currentReceiveAddress = wallet.currentReceiveAddress();
+        System.out.println("currentReceiveAddress_1: " + currentReceiveAddress.toString());
+        Address currentReceiveAddress_2 = wallet.currentReceiveAddress();
+        System.out.println("currentReceiveAddress_2: " + currentReceiveAddress_2.toString());
+        Address freshReceiveAddress = wallet.freshReceiveAddress();
+        System.out.println("freshReceiveAddress_1: " + freshReceiveAddress.toString());
+        Address freshReceiveAddress_2 = wallet.freshReceiveAddress();
+        System.out.println("freshReceiveAddress_2: " + freshReceiveAddress.toString());
+        Address currentChangeAddress = wallet.currentChangeAddress();
+        System.out.println("currentChangeAddress_1: " + currentChangeAddress.toString());
+        Address currentChangeAddress_2 = wallet.currentChangeAddress();
+        System.out.println("currentChangeAddress_2: " + currentChangeAddress.toString());
+        Address getChangeAddress = wallet.getChangeAddress();
+        System.out.println("getChangeAddress: " + getChangeAddress.toString());
+        List<Address> addressList = bitcoin.wallet().getIssuedReceiveAddresses();
+        boolean isRunning = bitcoin.isRunning();
+        Set<Transaction> transactionSet = bitcoin.wallet().getTransactions(false);
+        Iterator<Transaction> iterator = transactionSet.iterator();
+        while(iterator.hasNext()) {
+            Transaction transaction = iterator.next();
+            List<TransactionOutput> transactionOutputs = transaction.getOutputs();
+            for (TransactionOutput to : transactionOutputs){
+                Address address = to.getAddressFromP2PKHScript(params);
+                if (address != null){
+                    if (address.toString().equals("msZCnzzqqstKP5JRixNxPxw3oTgVuWxaVm"))
+                    System.out.println("getAddressFromP2PKHScript: " + address.toString());
+                }
+
+                Address address1 = to.getAddressFromP2SH(params);
+                if (address1 != null){
+                    if (address1.toString().equals("msZCnzzqqstKP5JRixNxPxw3oTgVuWxaVm"))
+                        System.out.println("getAddressFromP2SH: " + address1.toString());
+                }
+            }
+            String text = transaction.getHashAsString();
+            System.out.println("opreturn: " + text);
+        }
+
+    }
+
+}
